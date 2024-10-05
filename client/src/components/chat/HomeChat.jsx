8258e0f14790565efb7c00d3926237bdbc15ee86
kika1s1 <tamiratkebede120@gmail.com>
2024-10-05 23:48:07 +0300

    chatting with doctor full functionality

---
+++ client/src/components/chat/HomeChat.jsx
@@ -0,0 +1,89 @@
+import axios from 'axios'
+import  { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import {   Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { logout, setOnlineUser, setSocketConnection, setUser }from '../../features/chat/chatSlice'
+import Sidebar from './Sidebar'
+import io from 'socket.io-client'
+
+const HomeChat = () => {
+  // const user = useSelector(state => state.chat)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const location = useLocation()
+  useEffect(()=>{
+    const fetchUserDetails = async()=>{
+      try {
+          const URL = `${import.meta.env.VITE_APP_BACKEND_URL}/api/v1/users/user-details`
+          // console.log(URL)
+          const response = await axios({
+            url : URL,
+            withCredentials : true
+          })
+          // console.log("response,   hhfkjdfh",response)
+          dispatch(setUser(response.data.data))
+
+          if(response.data.data.logout){
+              dispatch(logout())
+              navigate("/signin")
+          }
+          // console.log("current user Details",response)
+      } catch (error) {
+          console.log("error",error)
+      }
+    }
+
+    fetchUserDetails()
+  },[dispatch, navigate])
+
+  /***socket connection */
+  useEffect(()=>{
+    const socketConnection = io(import.meta.env.VITE_APP_BACKEND_URL,{
+      auth : {
+        token : localStorage.getItem('token')
+      },
+    })
+
+    socketConnection.on('onlineUser',(data)=>{
+      // console.log(data)
+      dispatch(setOnlineUser(data))
+    })
+
+    dispatch(setSocketConnection(socketConnection))
+
+    return ()=>{
+      socketConnection.disconnect()
+    }
+  },[dispatch])
+
+  const basePath = location.pathname === '/livechat'
+  // console.log(basePath)
+
+  
+  
+  return (
+    <div >
+      <div className='grid lg:grid-cols-[300px,1fr] h-screen max-h-screen '>
+          <section className={`bg-white ${!basePath && "hidden"}  lg:block`}>
+             <Sidebar/>
+          </section>
+          {/**message component**/}
+        <section className={`${basePath && "hidden"}`} >
+            <Outlet/>
+        </section>
+          <div className={`flex  justify-center items-end flex-col gap-10  ${!basePath ? "hidden" : "lg:flex" } `}>
+              <div className=''>
+                <img
+                  src="https://www.salesmate.io/blog/wp-content/uploads/2021/11/20-Best-Live-Chat-Software-Services-to-Use-in-2021.jpg"
+                  className="rounded-full w-32 h-32 object-cover"
+                  alt='logo'
+                />
+              </div>
+              <p className='text-lg mt-2 text-slate-500'>Select user to send message</p>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomeChat
