770ae35b307fd1016a7e422077baf924c3d26521
kika1s1 <tamiratkebede120@gmail.com>
2024-10-06 16:11:08 +0300

    chatslice and userslice is added

---
+++ client/src/features/user/userSlice.js
@@ -0,0 +1,98 @@
+
+import { createSlice } from '@reduxjs/toolkit';
+const initialState = {
+  currentUser: null,
+  error: null,
+  loading: false,
+  profile:false,
+  showChat: false
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true;
+    },
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    signInFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+    updateUserStart: (state) => {
+      state.loading = true;
+    },
+    updateUserSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+    deleteUserStart: (state) => {
+      state.loading = true;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+    signOutUserStart: (state) => {
+      state.loading = true;
+    },
+    signOutUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    signOutUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+    setAndUnSetProfile: (state)=>{
+      state.profile = !state.profile
+    },
+    unSetProfile: (state)=>{
+      state.profile = false
+    },
+    setAndUnsetChat: (state)=>{
+      state.showChat = !state.showChat
+    },
+    unSetChat: (state)=>{
+      state.showChat = false
+    },
+
+  },
+});
+
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateUserFailure,
+  updateUserSuccess,
+  updateUserStart,
+  deleteUserFailure,
+  deleteUserSuccess,
+  deleteUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
+  signOutUserStart,
+  setAndUnSetProfile,
+  unSetProfile,
+  unSetChat, 
+  setAndUnsetChat
+} = userSlice.actions;
+
+export default userSlice.reducer;
